451d8868492104e4fd0d928275b8243e346f42e5
jed-richards <jed22richards@gmail.com>
2023-11-11 02:24:38 -0600

    solved

--- algorithms/rust/two-sum/two-sum.rs
+++ algorithms/rust/two-sum/two-sum.rs
@@ -1,4 +1,14 @@
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut ans: Vec<i32> = vec![0;2];
+        for i in 0..(nums.len()-1) {
+            for j in (i+1)..nums.len() {
+                if nums[i] + nums[j] == target {
+                    ans[0] = i as i32;
+                    ans[1] = j as i32;
+                }
+            }
+        }
+        return ans;
     }
 }
